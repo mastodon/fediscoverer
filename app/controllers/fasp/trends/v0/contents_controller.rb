fae5419c22908b1eb47a6e89bea99eb6aeba9b54
David Roetzel <david@roetzel.de>
2025-02-28 11:54:55 +0100

    Remove distribution.
    As per latest spec changes. This might not be gone forever,
    but should not be returned for now.

--- app/controllers/fasp/trends/v0/contents_controller.rb
+++ app/controllers/fasp/trends/v0/contents_controller.rb
@@ -1,8 +1,7 @@
 class Fasp::Trends::V0::ContentsController < Fasp::ApiController
   def index
     trending_content = ContentObject.trending(**trend_params)
-    distribution = ContentActivity.distribution_of(trending_content, hours: trend_params[:hours])
 
-    render json: { content: trending_content.map { |c| { uri: c.uri, rank: c.rank, distribution: distribution[c.id] } } }
+    render json: { content: trending_content.map { |c| { uri: c.uri, rank: c.rank } } }
   end
 end
--- app/controllers/fasp/trends/v0/links_controller.rb
+++ app/controllers/fasp/trends/v0/links_controller.rb
@@ -2,8 +2,7 @@ class Fasp::Trends::V0::LinksController < Fasp::ApiController
   def index
     trending_links = Link.includes(:recent_examples)
       .trending(**trend_params)
-    distribution = LinkActivity.distribution_of(trending_links, hours: trend_params[:hours], language: trend_params[:language])
 
-    render json: { links: trending_links.map { |l| { url: l.url, rank: l.rank, distribution: distribution[l.id], examples: l.recent_examples.map(&:uri) } } }
+    render json: { links: trending_links.map { |l| { url: l.url, rank: l.rank, examples: l.recent_examples.map(&:uri) } } }
   end
 end
