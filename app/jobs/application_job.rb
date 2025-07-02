4a88b9e3e5d0111758f17189dacf9e8cff0d70dd
David Roetzel <david@roetzel.de>
2025-07-02 12:20:50 +0200

    Retry jobs on HTTP 5xx errors
    And do not fail on 4xx errors

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,6 +2,9 @@ class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   retry_on ActiveRecord::Deadlocked
 
+  # Retry on HTTP errors
+  retry_on HTTPX::HTTPError, wait: :polynomially_longer
+
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError
 end
