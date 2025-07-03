387652bf9f3359db810ee899cd4d6447ad6afe6d
David Roetzel <david@roetzel.de>
2025-07-03 16:13:18 +0200

    Spread out job retries a bit more
    Servers might be unavailable, so no need to keep retrying with
    small delays. This should spread out retries over a little more
    than an hour.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,12 @@
 class ApplicationJob < ActiveJob::Base
+  # Spread out retries caused by HTTP 5xx errors
+  HTTP_5XX_RETRY_DELAY = ->(executions) { executions ** 5 + 30 }
+
   # Automatically retry jobs that encountered a deadlock
   retry_on ActiveRecord::Deadlocked
 
   # Retry on HTTP errors
-  retry_on HTTPX::HTTPError, wait: :polynomially_longer
+  retry_on HTTPX::HTTPError, wait: HTTP_500_RETRY_DELAY
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError
