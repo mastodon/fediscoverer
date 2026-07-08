67e5ab4d9e15859ca8cdee997af7076e1626772f
Pia B <pia.bregulla@web.de>
2026-07-08 14:51:57 +0200

    make rubocop happy

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,9 +1,9 @@
 # config/initializers/opentelemetry.rb
-require 'opentelemetry/sdk'
-require 'opentelemetry/instrumentation/all'
+require "opentelemetry/sdk"
+require "opentelemetry/instrumentation/all"
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'fediscoverer'
+  c.service_name = "fediscoverer"
   c.use_all()
 end
 
-FediscovererTracer = OpenTelemetry.tracer_provider.tracer('fediscoverer')
+FediscovererTracer = OpenTelemetry.tracer_provider.tracer("fediscoverer")
