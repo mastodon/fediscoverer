501d5bf3dab18ba730381d3a44310e370ec067ae
Pia B <pia.bregulla@web.de>
2026-07-09 14:32:59 +0200

    remove active job from open telemetry

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -5,7 +5,18 @@ require "opentelemetry-exporter-otlp"
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = "fediscoverer"
-  c.use_all()
+  # c.use_all() is not being used to exclude OpenTelemetry::Instrumentation::ActiveJob since this is already covered by sidekiq logging
+  c.use "OpenTelemetry::Instrumentation::ActiveSupport"
+  c.use "OpenTelemetry::Instrumentation::Rack"
+  c.use "OpenTelemetry::Instrumentation::ActionPack"
+  c.use "OpenTelemetry::Instrumentation::ActiveRecord"
+  c.use "OpenTelemetry::Instrumentation::ActionView"
+  c.use "OpenTelemetry::Instrumentation::ActionMailer"
+  c.use "OpenTelemetry::Instrumentation::HTTPX"
+  c.use "OpenTelemetry::Instrumentation::ConcurrentRuby"
+  c.use "OpenTelemetry::Instrumentation::Net::HTTP"
+  c.use "OpenTelemetry::Instrumentation::PG"
+  c.use "OpenTelemetry::Instrumentation::Rails"
 end
 
 FediscovererTracer = OpenTelemetry.tracer_provider.tracer("fediscoverer")
