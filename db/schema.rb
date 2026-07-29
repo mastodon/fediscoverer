54ae15f929ff18b4535ae324e7a6f9da572db33c
David Roetzel <david@roetzel.de>
2026-07-29 12:38:41 +0200

    Add some indexes
    to speed up trends calculation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_16_064856) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_29_101957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
 
   create_table "actor_languages", force: :cascade do |t|
     t.bigint "actor_id", null: false
-    t.string "language", null: false
     t.datetime "created_at", null: false
+    t.string "language", null: false
     t.datetime "updated_at", null: false
     t.index ["actor_id", "language"], name: "index_actor_languages_on_actor_id_and_language", unique: true
     t.index ["actor_id"], name: "index_actor_languages_on_actor_id"
   end
 
   create_table "actors", force: :cascade do |t|
-    t.bigint "server_id", null: false
-    t.string "uri", null: false
     t.string "actor_type", default: "Person", null: false
-    t.boolean "discoverable", default: false, null: false
-    t.boolean "indexable", default: false, null: false
-    t.text "full_text"
-    t.regconfig "pg_text_search_configuration", default: "english", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "followers_count", default: 0, null: false
-    t.boolean "recommended", default: false, null: false
     t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.boolean "discoverable", default: false, null: false
+    t.integer "followers_count", default: 0, null: false
+    t.text "full_text"
+    t.boolean "indexable", default: false, null: false
+    t.regconfig "pg_text_search_configuration", default: "english", null: false
+    t.boolean "recommended", default: false, null: false
+    t.bigint "server_id", null: false
+    t.datetime "updated_at", null: false
+    t.string "uri", null: false
     t.index "to_tsvector(pg_text_search_configuration, full_text)", name: "actors_description_idx", using: :gin
     t.index ["server_id"], name: "index_actors_on_server_id"
     t.index ["uri"], name: "index_actors_on_uri", unique: true
@@ -44,46 +44,47 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_16_064856) do
 
   create_table "content_activities", force: :cascade do |t|
     t.bigint "content_object_id", null: false
+    t.datetime "created_at", null: false
     t.datetime "hour_of_activity", null: false
-    t.integer "shares", default: 0, null: false
     t.integer "likes", default: 0, null: false
     t.integer "replies", default: 0, null: false
     t.float "score", default: 0.0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "shares", default: 0, null: false
     t.integer "trend_signals", default: 0, null: false
+    t.datetime "updated_at", null: false
     t.index ["content_object_id", "hour_of_activity"], name: "idx_on_content_object_id_hour_of_activity_6ed57d161f", unique: true
     t.index ["content_object_id"], name: "index_content_activities_on_content_object_id"
+    t.index ["hour_of_activity"], name: "index_content_activities_on_hour_of_activity"
   end
 
   create_table "content_objects", force: :cascade do |t|
     t.bigint "actor_id", null: false
-    t.string "uri", null: false
-    t.string "object_type", null: false
-    t.datetime "published_at", null: false
-    t.datetime "last_edited_at", null: false
-    t.boolean "sensitive", null: false
-    t.string "language"
+    t.integer "attached_audio", default: 0, null: false
     t.integer "attached_images", default: 0, null: false
     t.integer "attached_videos", default: 0, null: false
-    t.integer "attached_audio", default: 0, null: false
-    t.integer "replies", default: 0, null: false
-    t.integer "likes", default: 0, null: false
-    t.integer "shares", default: 0, null: false
-    t.text "full_text"
-    t.regconfig "pg_text_search_configuration", default: "english", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "full_text"
+    t.string "language"
+    t.datetime "last_edited_at", null: false
+    t.integer "likes", default: 0, null: false
+    t.string "object_type", null: false
+    t.regconfig "pg_text_search_configuration", default: "english", null: false
+    t.datetime "published_at", null: false
+    t.integer "replies", default: 0, null: false
+    t.boolean "sensitive", null: false
+    t.integer "shares", default: 0, null: false
     t.integer "trend_signals", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.string "uri", null: false
     t.index "to_tsvector(pg_text_search_configuration, full_text)", name: "content_objects_full_text_idx", using: :gin
     t.index ["actor_id"], name: "index_content_objects_on_actor_id"
     t.index ["uri"], name: "index_content_objects_on_uri", unique: true
   end
 
   create_table "fasp_base_admin_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "password_digest"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_fasp_base_admin_users_on_email", unique: true
   end
@@ -97,88 +98,89 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_16_064856) do
 
   create_table "fasp_base_servers", force: :cascade do |t|
     t.string "base_url", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.json "enabled_capabilities"
     t.string "fasp_private_key_pem", null: false
     t.string "fasp_remote_id"
     t.string "public_key_pem"
     t.string "registration_completion_uri"
-    t.json "enabled_capabilities"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["base_url"], name: "index_fasp_base_servers_on_base_url", unique: true
     t.index ["user_id"], name: "index_fasp_base_servers_on_user_id"
   end
 
   create_table "fasp_base_settings", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "value"
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
+    t.string "value"
     t.index ["name"], name: "index_fasp_base_settings_on_name", unique: true
   end
 
   create_table "fasp_base_users", force: :cascade do |t|
+    t.boolean "active", default: true, null: false
+    t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "password_digest"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "active", default: true, null: false
     t.index ["email"], name: "index_fasp_base_users_on_email", unique: true
   end
 
   create_table "fasp_data_sharing_actors", force: :cascade do |t|
-    t.text "private_key_pem", null: false
     t.datetime "created_at", null: false
+    t.text "private_key_pem", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "fasp_data_sharing_backfill_requests", force: :cascade do |t|
-    t.bigint "fasp_base_server_id", null: false
-    t.string "remote_id", null: false
     t.string "category", null: false
-    t.integer "max_count"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "fasp_base_server_id", null: false
     t.boolean "fulfilled", default: false, null: false
+    t.integer "max_count"
+    t.string "remote_id", null: false
+    t.datetime "updated_at", null: false
     t.index ["fasp_base_server_id"], name: "idx_on_fasp_base_server_id_0f3fe7f51e"
   end
 
   create_table "fasp_data_sharing_subscriptions", force: :cascade do |t|
-    t.bigint "fasp_base_server_id", null: false
-    t.string "remote_id", null: false
     t.string "category", null: false
-    t.string "subscription_type", null: false
+    t.datetime "created_at", null: false
+    t.bigint "fasp_base_server_id", null: false
     t.integer "max_batch_size"
-    t.integer "threshold_timeframe"
-    t.integer "threshold_shares"
+    t.string "remote_id", null: false
+    t.string "subscription_type", null: false
     t.integer "threshold_likes"
     t.integer "threshold_replies"
-    t.datetime "created_at", null: false
+    t.integer "threshold_shares"
+    t.integer "threshold_timeframe"
     t.datetime "updated_at", null: false
     t.index ["fasp_base_server_id"], name: "index_fasp_data_sharing_subscriptions_on_fasp_base_server_id"
   end
 
   create_table "hashtag_activities", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "distinct_users", default: 0, null: false
     t.bigint "hashtag_id", null: false
     t.datetime "hour_of_activity", null: false
-    t.integer "total_uses", default: 0, null: false
-    t.integer "distinct_users", default: 0, null: false
-    t.integer "shares", default: 0, null: false
+    t.string "language"
     t.integer "likes", default: 0, null: false
     t.integer "replies", default: 0, null: false
     t.float "score", default: 0.0, null: false
-    t.string "language"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "shares", default: 0, null: false
+    t.integer "total_uses", default: 0, null: false
     t.integer "trend_signals", default: 0, null: false
+    t.datetime "updated_at", null: false
     t.index ["hashtag_id", "hour_of_activity", "language"], name: "idx_on_hashtag_id_hour_of_activity_language_65749e4bf4", unique: true
     t.index ["hashtag_id"], name: "index_hashtag_activities_on_hashtag_id"
+    t.index ["hour_of_activity", "language"], name: "index_hashtag_activities_on_hour_of_activity_and_language"
   end
 
   create_table "hashtag_usages", force: :cascade do |t|
     t.bigint "content_object_id", null: false
-    t.bigint "hashtag_id", null: false
     t.datetime "created_at", null: false
+    t.bigint "hashtag_id", null: false
     t.datetime "updated_at", null: false
     t.index ["content_object_id", "hashtag_id"], name: "index_hashtag_usages_on_content_object_id_and_hashtag_id", unique: true
     t.index ["content_object_id"], name: "index_hashtag_usages_on_content_object_id"
@@ -186,33 +188,34 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_16_064856) do
   end
 
   create_table "hashtags", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_hashtags_on_name", unique: true
   end
 
   create_table "link_activities", force: :cascade do |t|
-    t.bigint "link_id", null: false
-    t.datetime "hour_of_activity", null: false
-    t.integer "total_uses", default: 0, null: false
+    t.datetime "created_at", null: false
     t.integer "distinct_users", default: 0, null: false
-    t.integer "shares", default: 0, null: false
+    t.datetime "hour_of_activity", null: false
+    t.string "language"
     t.integer "likes", default: 0, null: false
+    t.bigint "link_id", null: false
     t.integer "replies", default: 0, null: false
     t.float "score", default: 0.0, null: false
-    t.string "language"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "shares", default: 0, null: false
+    t.integer "total_uses", default: 0, null: false
     t.integer "trend_signals", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.index ["hour_of_activity", "language"], name: "index_link_activities_on_hour_of_activity_and_language"
     t.index ["link_id", "hour_of_activity", "language"], name: "idx_on_link_id_hour_of_activity_language_ca2e4ae168", unique: true
     t.index ["link_id"], name: "index_link_activities_on_link_id"
   end
 
   create_table "link_usages", force: :cascade do |t|
     t.bigint "content_object_id", null: false
-    t.bigint "link_id", null: false
     t.datetime "created_at", null: false
+    t.bigint "link_id", null: false
     t.datetime "updated_at", null: false
     t.index ["content_object_id", "link_id"], name: "index_link_usages_on_content_object_id_and_link_id", unique: true
     t.index ["content_object_id"], name: "index_link_usages_on_content_object_id"
@@ -220,21 +223,21 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_16_064856) do
   end
 
   create_table "links", force: :cascade do |t|
-    t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url", null: false
     t.index ["url"], name: "index_links_on_url", unique: true
   end
 
   create_table "servers", force: :cascade do |t|
-    t.string "domain_name", null: false
     t.boolean "available", default: true, null: false
-    t.datetime "last_queried_at"
-    t.integer "connection_failures", default: 0, null: false
-    t.datetime "pause_until"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "blocked", default: false, null: false
+    t.integer "connection_failures", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.string "domain_name", null: false
+    t.datetime "last_queried_at"
+    t.datetime "pause_until"
+    t.datetime "updated_at", null: false
     t.index ["domain_name"], name: "index_servers_on_domain_name", unique: true
   end
 
