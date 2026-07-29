e0521fb105d0e00934d62164b42d8463cc8b6b98
David Roetzel <david@roetzel.de>
2026-07-29 12:38:41 +0200

    Add some indexes
    to speed up trends calculation.

---
+++ db/migrate/20260729101957_add_indexes_to_activities.rb
@@ -0,0 +1,7 @@
+class AddIndexesToActivities < ActiveRecord::Migration[8.1]
+  def change
+    add_index :content_activities, :hour_of_activity
+    add_index :hashtag_activities, [:hour_of_activity, :language]
+    add_index :link_activities, [:hour_of_activity, :language]
+  end
+end
