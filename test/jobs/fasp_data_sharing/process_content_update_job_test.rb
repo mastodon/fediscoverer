a50e49f8f44371e00f9e027c1edbe839f63d6bf1
Pia B <pia.bregulla@web.de>
2026-09-07 14:42:37 +0200

    refactor test

--- test/jobs/fasp_data_sharing/process_content_update_job_test.rb
+++ test/jobs/fasp_data_sharing/process_content_update_job_test.rb
@@ -4,7 +4,6 @@ module FaspDataSharing
   class ProcessContentUpdateJobTest < ActiveJob::TestCase
     setup do
       @uri = "https://mastodon.example.com/status/4711"
-      mock_valid_content_request(uri: @uri)
       @job = ProcessContentUpdateJob.new
       @actor_uri = "https://unknown.example.com/users/NewActor"
     end
@@ -18,25 +17,13 @@ module FaspDataSharing
       assert(job.perform_now)
     end
 
-    test "does not enqueue job when content object is deleted before enqueing update" do
-      mock_valid_actor_request(uri: @actor_uri)
-      mock_valid_content_request(uri: @uri, actor: @actor_uri)
-      RetrieveContentJob.new.perform(@uri)
-      ContentObject.where(uri: @uri).first.destroy
-
+    test "does not enqueue job when content object is not existent" do
       assert_enqueued_jobs(0) do
         @job.perform(@uri)
       end
-    end
-
-    test "does not create a new object, and returns from job when content object cannot be found on update" do
-      mock_valid_actor_request(uri: @actor_uri)
-      mock_valid_content_request(uri: @uri, actor: @actor_uri)
-      RetrieveContentJob.new.perform(@uri)
       queue = @job.perform(@uri)
-      ContentObject.where(uri: @uri).first.destroy
 
-      assert_nil queue.perform_now
+      assert_nil queue
       assert ContentObject.where(uri: @uri).blank?
     end
   end
